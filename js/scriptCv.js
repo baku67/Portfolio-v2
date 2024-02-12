51ecb175f9ca870858712cd00f2efbcb3f110c52
baku67 <basile08@hotmail.fr>
2024-02-12 20:48:37 +0100

    Timeout() observer anims  /cv mobile + Redim Shapes mobile

--- js/scriptCv.js
+++ js/scriptCv.js
@@ -252,34 +252,45 @@ window.onload = function() {
     })
 
 
+    // Delai Animations fadeIn sur mobile:
+    if(mobileDetection) {
+        var delaiAnimMobile = 400;
+    }
+    else {
+        var delaiAnimMobile = 0;
+    }
+    
 
 
     // Observer diplome <Formation/> CV
     const observerFormations = new IntersectionObserver(entries => {
-        // Loop over the entries
         entries.forEach(entry => {
-            // If the element is visible
+
             if (entry.isIntersecting) {
 
-                document.getElementById("formations").classList.add("formationsAnim")
+                setTimeout(() => {
+                    document.getElementById("formations").classList.add("formationsAnim")
 
-                // Draw du chapeau diplome <Formation/> CV
-                var formationPathes = document.querySelectorAll('.formationSvg path');
-                formationPathes.forEach(element => {
-                    var lengthRec = element.getTotalLength();
-                    element.style.transition = element.style.WebkitTransition = 'none';
-                    element.style.strokeDasharray = lengthRec + ' ' + lengthRec;
-                    element.style.strokeDashoffset = lengthRec;
-                    element.getBoundingClientRect();
-                    element.style.transition = element.style.WebkitTransition =
-                    'stroke-dashoffset 2.5s 0.5s ease-in-out';
-                    element.style.strokeDashoffset = '0';    
-                });
+                    // Draw du chapeau diplome <Formation/> CV
+                    var formationPathes = document.querySelectorAll('.formationSvg path');
+                    formationPathes.forEach(element => {
+                        var lengthRec = element.getTotalLength();
+                        element.style.transition = element.style.WebkitTransition = 'none';
+                        element.style.strokeDasharray = lengthRec + ' ' + lengthRec;
+                        element.style.strokeDashoffset = lengthRec;
+                        element.getBoundingClientRect();
+                        element.style.transition = element.style.WebkitTransition =
+                        'stroke-dashoffset 2.5s 0.5s ease-in-out';
+                        element.style.strokeDashoffset = '0';    
+                    });
+    
+                    setTimeout(function() {
+                        document.getElementById("formations").style.opacity = "1";
+                        document.getElementById("formations").classList.remove("formationsAnim")
+                    }, 2001)
 
-                setTimeout(function() {
-                    document.getElementById("formations").style.opacity = "1";
-                    document.getElementById("formations").classList.remove("formationsAnim")
-                }, 2001)
+                }, delaiAnimMobile);
+
             }
             else {
                 document.getElementById("formations").style.opacity = "0";
@@ -292,30 +303,32 @@ window.onload = function() {
 
     // Observer checkbox <Compétences/> CV
     const observerCompetences = new IntersectionObserver(entries => {
-        // Loop over the entries
         entries.forEach(entry => {
-            // If the element is visible
+
             if (entry.isIntersecting) {
 
-                document.getElementById("competences").classList.add("competencesAnim")
+                setTimeout(() => {
+                    document.getElementById("competences").classList.add("competencesAnim")
 
-                // Draw du checkbox <Compétences/> CV
-                var skillsPathes = document.querySelectorAll('.skillsSvg path');
-                skillsPathes.forEach(element => {
-                    var lengthRec2 = element.getTotalLength();
-                    element.style.transition = element.style.WebkitTransition = 'none';
-                    element.style.strokeDasharray = lengthRec2 + ' ' + lengthRec2;
-                    element.style.strokeDashoffset = lengthRec2;
-                    element.getBoundingClientRect();
-                    element.style.transition = element.style.WebkitTransition =
-                    'stroke-dashoffset 2.5s 0.5s ease-in-out';
-                    element.style.strokeDashoffset = '0';    
-                });
+                    // Draw du checkbox <Compétences/> CV
+                    var skillsPathes = document.querySelectorAll('.skillsSvg path');
+                    skillsPathes.forEach(element => {
+                        var lengthRec2 = element.getTotalLength();
+                        element.style.transition = element.style.WebkitTransition = 'none';
+                        element.style.strokeDasharray = lengthRec2 + ' ' + lengthRec2;
+                        element.style.strokeDashoffset = lengthRec2;
+                        element.getBoundingClientRect();
+                        element.style.transition = element.style.WebkitTransition =
+                        'stroke-dashoffset 2.5s 0.5s ease-in-out';
+                        element.style.strokeDashoffset = '0';    
+                    });
 
-                setTimeout(function() {
-                    document.getElementById("competences").style.opacity = "1";
-                    document.getElementById("competences").classList.remove("competencesAnim")
-                }, 2001)
+                    setTimeout(function() {
+                        document.getElementById("competences").style.opacity = "1";
+                        document.getElementById("competences").classList.remove("competencesAnim")
+                    }, 2001)
+
+                }, delaiAnimMobile);
             }
             else {
                 // document.getElementById("competences").classList.remove("competencesAnim")
@@ -326,32 +339,37 @@ window.onload = function() {
     observerCompetences.observe(document.querySelector('#cvSectionBgGradientSkills'));
     
 
+
+
     // Observer poisson <Hobbies/> CV
     const observerHobbies = new IntersectionObserver(entries => {
-        // Loop over the entries
         entries.forEach(entry => {
-            // If the element is visible
+
             if (entry.isIntersecting) {
 
-                document.getElementById("hobbies").classList.add("hobbiesAnim")
+                setTimeout(() => {
 
-                // Draw du poisson <Hobbies/> CV
-                var hobbiesPathes = document.querySelectorAll('.hobbiesSvg path');
-                hobbiesPathes.forEach(element => {
-                    var lengthRec3 = element.getTotalLength();
-                    element.style.transition = element.style.WebkitTransition = 'none';
-                    element.style.strokeDasharray = lengthRec3 + ' ' + lengthRec3;
-                    element.style.strokeDashoffset = lengthRec3;
-                    element.getBoundingClientRect();
-                    element.style.transition = element.style.WebkitTransition =
-                    'stroke-dashoffset 2.5s 0.5s ease-in-out';
-                    element.style.strokeDashoffset = '0';    
-                });
+                    document.getElementById("hobbies").classList.add("hobbiesAnim")
 
-                setTimeout(function() {
-                    document.getElementById("hobbies").style.opacity = "1";
-                    document.getElementById("hobbies").classList.remove("hobbiesAnim")
-                }, 2001)
+                    // Draw du poisson <Hobbies/> CV
+                    var hobbiesPathes = document.querySelectorAll('.hobbiesSvg path');
+                    hobbiesPathes.forEach(element => {
+                        var lengthRec3 = element.getTotalLength();
+                        element.style.transition = element.style.WebkitTransition = 'none';
+                        element.style.strokeDasharray = lengthRec3 + ' ' + lengthRec3;
+                        element.style.strokeDashoffset = lengthRec3;
+                        element.getBoundingClientRect();
+                        element.style.transition = element.style.WebkitTransition =
+                        'stroke-dashoffset 2.5s 0.5s ease-in-out';
+                        element.style.strokeDashoffset = '0';    
+                    });
+
+                    setTimeout(function() {
+                        document.getElementById("hobbies").style.opacity = "1";
+                        document.getElementById("hobbies").classList.remove("hobbiesAnim")
+                    }, 2001)
+
+                }, delaiAnimMobile);
             }
             else {
                 document.getElementById("hobbies").style.opacity = "0";
@@ -362,58 +380,36 @@ window.onload = function() {
     
 
 
-    
-
-  // Fixed menuBörgir lors scroll (mobile), equivalent à la sideNav PC
-  if(mobileDetection) {
-      
-    const observerHeaderBorger = new IntersectionObserver(entries => {
-
-      entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            document.getElementById("openBtn2").style.display = "none";
-        }
-        else {
-            document.getElementById("openBtn2").style.display = "block";
-        }
-      })
-    })
-    observerHeaderBorger.observe(document.querySelector('#headerCv'));
-}
-
-
-
-
-
-
 
     // Observer bulles discussion <Langues/> CV
     const observerLang = new IntersectionObserver(entries => {
-        // Loop over the entries
         entries.forEach(entry => {
-            // If the element is visible
+
             if (entry.isIntersecting) {
 
-                document.getElementById("langues").classList.add("langsAnim")
+                setTimeout(() => {
 
-                // Draw bulles discussion <Langues/> CV
-                var langsPathes = document.querySelectorAll('.langsSvg path');
-                langsPathes.forEach(element => {
-                    var lengthRec5 = element.getTotalLength();
-                    element.style.transition = element.style.WebkitTransition = 'none';
-                    element.style.strokeDasharray = lengthRec5 + ' ' + lengthRec5;
-                    element.style.strokeDashoffset = lengthRec5;
-                    element.getBoundingClientRect();
-                    element.style.transition = element.style.WebkitTransition =
-                    'stroke-dashoffset 2.5s 0.1s ease-in-out';
-                    element.style.strokeDashoffset = '0';    
-                });
+                    document.getElementById("langues").classList.add("langsAnim")
 
-                setTimeout(function() {
-                    document.getElementById("langues").style.opacity = "1";
-                    document.getElementById("langues").classList.remove("langsAnim")
-                }, 2001)
+                    // Draw bulles discussion <Langues/> CV
+                    var langsPathes = document.querySelectorAll('.langsSvg path');
+                    langsPathes.forEach(element => {
+                        var lengthRec5 = element.getTotalLength();
+                        element.style.transition = element.style.WebkitTransition = 'none';
+                        element.style.strokeDasharray = lengthRec5 + ' ' + lengthRec5;
+                        element.style.strokeDashoffset = lengthRec5;
+                        element.getBoundingClientRect();
+                        element.style.transition = element.style.WebkitTransition =
+                        'stroke-dashoffset 2.5s 0.1s ease-in-out';
+                        element.style.strokeDashoffset = '0';    
+                    });
+
+                    setTimeout(function() {
+                        document.getElementById("langues").style.opacity = "1";
+                        document.getElementById("langues").classList.remove("langsAnim")
+                    }, 2001)
+
+                }, delaiAnimMobile);
             }
             else {
                 document.getElementById("langues").style.opacity = "0";
@@ -425,32 +421,38 @@ window.onload = function() {
 
 
 
+
+
+
     // Observer clée à molette <Outils/> CV
     const observerOutils = new IntersectionObserver(entries => {
-        // Loop over the entries
         entries.forEach(entry => {
-          // If the element is visible
+
           if (entry.isIntersecting) {
 
-            document.getElementById("outils").classList.add("outilsAnim")
+            setTimeout(() => {
 
-                // Draw clée à molette <Outils/> CV
-                var outilsPathes = document.querySelectorAll('.outilsSvg path');
-                outilsPathes.forEach(element => {
-                    var lengthRec4 = element.getTotalLength();
-                    element.style.transition = element.style.WebkitTransition = 'none';
-                    element.style.strokeDasharray = lengthRec4 + ' ' + lengthRec4;
-                    element.style.strokeDashoffset = lengthRec4;
-                    element.getBoundingClientRect();
-                    element.style.transition = element.style.WebkitTransition =
-                    'stroke-dashoffset 2.5s 0.5s ease-in-out';
-                    element.style.strokeDashoffset = '0';    
-                });
+                document.getElementById("outils").classList.add("outilsAnim")
 
-            setTimeout(function() {
-                document.getElementById("outils").style.opacity = "1";
-                document.getElementById("outils").classList.remove("outilsAnim")
-            }, 2001)
+                    // Draw clée à molette <Outils/> CV
+                    var outilsPathes = document.querySelectorAll('.outilsSvg path');
+                    outilsPathes.forEach(element => {
+                        var lengthRec4 = element.getTotalLength();
+                        element.style.transition = element.style.WebkitTransition = 'none';
+                        element.style.strokeDasharray = lengthRec4 + ' ' + lengthRec4;
+                        element.style.strokeDashoffset = lengthRec4;
+                        element.getBoundingClientRect();
+                        element.style.transition = element.style.WebkitTransition =
+                        'stroke-dashoffset 2.5s 0.5s ease-in-out';
+                        element.style.strokeDashoffset = '0';    
+                    });
+
+                setTimeout(function() {
+                    document.getElementById("outils").style.opacity = "1";
+                    document.getElementById("outils").classList.remove("outilsAnim")
+                }, 2001)
+
+            }, delaiAnimMobile);
           }
           else {
             document.getElementById("outils").style.opacity = "0";
@@ -460,6 +462,26 @@ window.onload = function() {
       observerOutils.observe(document.querySelector('#outils'));
     
 
+
+
+            
+    // Fixed menuBörgir lors scroll (mobile), equivalent à la sideNav PC
+    if(mobileDetection) {
+        
+        const observerHeaderBorger = new IntersectionObserver(entries => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                document.getElementById("openBtn2").style.display = "none";
+            }
+            else {
+                document.getElementById("openBtn2").style.display = "block";
+            }
+        })
+        })
+        observerHeaderBorger.observe(document.querySelector('#headerCv'));
+    }
 
 
 }
